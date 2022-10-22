ee322da0d1ee2dc2611770544da5ef89f56d1c63
blackrd1 <92227352+blackrd1@users.noreply.github.com>
2022-10-22 15:22:34 -0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,4 @@
+function add_fields() {
+var table = document.getElementById("myTable");
+var row = table.insertRow(-1).innerHTML = '<tr><td><textarea name ="Episode" placeholder="Episode" th: field = "${questionAnswerSet.question}" id="question" style = "resize: none; width:100%;"></textarea></td><td><textarea name="Podcast" placeholder ="Podcast" th: field = "${questionAnswerSet.answer}" id="answer" style="resize:none;width: 100%;"></textarea></td ><td><textarea name="Minutes" placeholder ="Minutes" th: field = "${questionAnswerSet.answer}" id="answer" style="resize:none;width: 100%;"></textarea></td ><td><button class="save"> Save </button><button class="edit"> Edit </button><button class="delete"> Delete </button></td></tr>';
+}
